2c7c6848e8084e870758da6fbe65fd346543bbfc
Harold Alcantara <harold_633@hotmail.com>
2020-03-19 18:36:58 -0500

    dropdown

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+(function() {
+   
+    document.querySelector('.nav-button').addEventListener('click', function() {
+     
+      this.parentNode.parentNode.classList.toggle('closed')
+    }, false);
+  })();
